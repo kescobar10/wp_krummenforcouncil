ff82e9fd674a85b3afebe9c738f1c22a52ff808e
Kenia Escobar <keniarescobar@gmail.com>
2017-11-12 16:52:36 -0500

    add archive, category and tag pages

--- single.php
+++ single.php
@@ -1,71 +1,42 @@
 <?php get_header(); ?>
 
-	<main role="main">
-	<!-- section -->
-	<section>
+<section id="inner-banner">
+	<img src="<?php bloginfo('template_url'); ?>/assets/img/signs.jpg" alt="Naperville" />
+	<h4><span>News &amp;<span id="naperville">&nbsp;Updates</span></span></h4>
+</section>
 
-	<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
-		<!-- article -->
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<section class="issues-wrapper">
+	<div class="issues-left">
+		<time datetime="2017-07-14">July 14, 2017</time>
+		<h3><?php the_category(); ?></h3>
+		<h2><?php the_title(); ?></h2>
+		<?php the_content(); ?>
+	</div>
+	<div class="widget-right">
+		<div class="contact-card">
+			<h3>Newsletter</h3>
+			<form>
+				Name:<br>
+				<input type="text" name="fullname"><br>
+				Email:<br>
+				<input type="text" name="fullname"><br>
+				Zip Code:<br>
+				<input type="text" name="fullname"><br>
+				<input class="submit" type="submit" value="Submit">
+			</form>
+		</div>
+	</div>
 
-			<!-- post thumbnail -->
-			<?php if ( has_post_thumbnail()) : // Check if Thumbnail exists ?>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
-					<?php the_post_thumbnail(); // Fullsize image for the single post ?>
-				</a>
-			<?php endif; ?>
-			<!-- /post thumbnail -->
+</section>
+<!--- content end --->
+<?php endwhile; endif; ?>
 
-			<!-- post title -->
-			<h1>
-				<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
-			</h1>
-			<!-- /post title -->
-
-			<!-- post details -->
-			<span class="date">
-				<time datetime="<?php the_time('Y-m-d'); ?> <?php the_time('H:i'); ?>">
-					<?php the_date(); ?> <?php the_time(); ?>
-				</time>
-			</span>
-			<span class="author"><?php _e( 'Published by', 'html5blank' ); ?> <?php the_author_posts_link(); ?></span>
-			<span class="comments"><?php if (comments_open( get_the_ID() ) ) comments_popup_link( __( 'Leave your thoughts', 'html5blank' ), __( '1 Comment', 'html5blank' ), __( '% Comments', 'html5blank' )); ?></span>
-			<!-- /post details -->
-
-			<?php the_content(); // Dynamic Content ?>
-
-			<?php the_tags( __( 'Tags: ', 'html5blank' ), ', ', '<br>'); // Separated by commas with a line break at the end ?>
-
-			<p><?php _e( 'Categorised in: ', 'html5blank' ); the_category(', '); // Separated by commas ?></p>
-
-			<p><?php _e( 'This post was written by ', 'html5blank' ); the_author(); ?></p>
-
-			<?php edit_post_link(); // Always handy to have Edit Post Links available ?>
-
-			<?php comments_template(); ?>
-
-		</article>
-		<!-- /article -->
-
-	<?php endwhile; ?>
-
-	<?php else: ?>
-
-		<!-- article -->
-		<article>
-
-			<h1><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h1>
-
-		</article>
-		<!-- /article -->
-
-	<?php endif; ?>
-
-	</section>
-	<!-- /section -->
-	</main>
-
-<?php get_sidebar(); ?>
+<section>
+	<figure id="logo-2">
+		<a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" alt="John Krummen for Council" /></a>
+	</figure>
+</section>
 
 <?php get_footer(); ?>
--- tag.php
+++ tag.php
@@ -1,19 +1,54 @@
 <?php get_header(); ?>
 
-	<main role="main">
-		<!-- section -->
-		<section>
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
+	  <section class="inner-main">
 
-			<?php get_template_part('partials/loop'); ?>
+		  <?php if (have_posts()) : ?>
+			  <div>
+				  <h2><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h2>
+				  <?php while (have_posts()) : the_post();?>
 
-			<?php get_template_part('partials/pagination'); ?>
+			  <time datetime="2017-07-14"><?php echo get_the_date(); ?></time>
+			  <h2><a href="<?php the_permalink(); ?>" class="blog-link"><?php the_title(); ?></a></h2>
+			  <h3 class="category">
 
-		</section>
-		<!-- /section -->
-	</main>
+				  <?php
+				  $categories = get_the_category();
 
-<?php get_sidebar(); ?>
+				  if ($categories) {
+
+					  $i = 1;
+					  foreach ($categories as $one) {
+						  echo $one->name;
+
+						  if ( count($categories) > 0 && count($categories) > $i ) {
+							  echo ', ';
+						  }
+						  $i++;
+					  }
+
+				  }
+
+				  ?>
+			  </h3>
+			  <p>John is an independent engineering consultant, working with a variety of companies, including multiple Fortune 500 companies. John’s first job out of college was driving corvettes around the GM Proving Grounds. John has published multiple technical papers on advanced engineering topics, and is a patent-holding engineer. John is also a technical project manager, having installed capital equipment at nuclear facilities.</p>
+			  <button class="read-more"><a href="<?php the_permalink(); ?>">Read More</a></button>
+
+		  		<?php endwhile; ?>
+
+  			</div>
+
+  <?php else: ?>
+	  <br>
+	  <h2>No Posts Found</h2>
+	  <?php endif; ?>
+
+	</section>
+
+	  <section>
+		  <figure id="logo-2">
+			  <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" alt="John Krummen for Council" /></a>
+		  </figure>
+	  </section>
 
 <?php get_footer(); ?>
