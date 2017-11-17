590893241b5796bcd45c4c94a403b6ed06687d1d
Kenia Escobar <keniarescobar@gmail.com>
2017-11-17 13:28:21 -0500

    fix page names for cpt and sidebars

--- functions/custom-post-types.php
+++ functions/custom-post-types.php
@@ -66,7 +66,7 @@ function create_post_type_html5()
         ),
         'public' => true,
         'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-        'has_archive' => false,
+        'has_archive' => true,
         'supports' => array(
             'title',
             'editor',
@@ -80,44 +80,6 @@ function create_post_type_html5()
         'issue',             // Object Type
         array(
             'label' => __( 'Type of Issue' ),
-            'rewrite' => array( 'slug' => 'type' ),
-            'hierarchical' => true, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
-        )
-    );
-
-    register_post_type('html5-blank', // Register Custom Post Type
-        array(
-        'labels' => array(
-            'name' => __('HTML5 Blank Custom Post', 'html5blank'), // Rename these to suit
-            'singular_name' => __('HTML5 Blank Custom Post', 'html5blank'),
-            'add_new' => __('Add New', 'html5blank'),
-            'add_new_item' => __('Add New HTML5 Blank Custom Post', 'html5blank'),
-            'edit' => __('Edit', 'html5blank'),
-            'edit_item' => __('Edit HTML5 Blank Custom Post', 'html5blank'),
-            'new_item' => __('New HTML5 Blank Custom Post', 'html5blank'),
-            'view' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'view_item' => __('View HTML5 Blank Custom Post', 'html5blank'),
-            'search_items' => __('Search HTML5 Blank Custom Post', 'html5blank'),
-            'not_found' => __('No HTML5 Blank Custom Posts found', 'html5blank'),
-            'not_found_in_trash' => __('No HTML5 Blank Custom Posts found in Trash', 'html5blank')
-        ),
-        'public' => true,
-        'hierarchical' => true, // Allows your posts to behave like Hierarchy Pages
-        'has_archive' => true,
-        'supports' => array(
-            'title',
-            'editor',
-            'excerpt',
-            'thumbnail'
-        ), // Go to Dashboard Custom HTML5 Blank post for supports
-        'menu_icon' => 'dashicons-format-status',   // http://www.kevinleary.net/wordpress-dashicons-list-custom-post-type-icons/
-        'can_export' => true // Allows export in Tools > Export
-    ));
-    register_taxonomy(
-        'type_of_staff',    // Taxonomy
-        'staff',             // Object Type
-        array(
-            'label' => __( 'Type' ),
             'rewrite' => array( 'slug' => 'type' ),
             'hierarchical' => true, // Is this taxonomy hierarchical like categories or not hierarchical like tags.
         )
