c5203ca6e4061525f297234bd01d8a6a33775be4
Kenia Escobar <keniarescobar@gmail.com>
2017-12-02 21:21:49 -0500

    add a class for styling

--- search.php
+++ search.php
@@ -2,9 +2,9 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="inner-main search-results">
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
+			<h2 class="search-results"><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h2>
 
 			<?php get_template_part('partials/loop'); ?>
 
@@ -13,7 +13,5 @@
 		</section>
 		<!-- /section -->
 	</main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
