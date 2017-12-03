582deb31bc3ad8a5425e35f100b7d42d978ce252
Kenia Escobar <keniarescobar@gmail.com>
2017-12-02 21:17:25 -0500

    update the class and heading

--- partials/searchform.php
+++ partials/searchform.php
@@ -1,5 +1,6 @@
 <!-- search -->
-<form class="search" method="get" action="<?php echo home_url(); ?>" role="search">
+<form class="search" method="get" action="<?php echo home_url(); ?>" role="search" id="search-box">
+	<h5>Search</h5>
 	<input class="search-input" type="search" name="s" placeholder="<?php _e( 'To search, type and hit enter.', 'html5blank' ); ?>">
 	<button class="search-submit" type="submit" role="button"><?php _e( 'Search', 'html5blank' ); ?></button>
 </form>
