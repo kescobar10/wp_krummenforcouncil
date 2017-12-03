1a057c1e36d6d612fd3a60b711d2ca630970b1ce
Kenia Escobar <keniarescobar@gmail.com>
2017-12-02 21:18:08 -0500

    add classes for styling

--- 404.php
+++ 404.php
@@ -2,15 +2,16 @@
 
 	<main role="main">
 		<!-- section -->
-		<section>
+		<section class="inner-main">
 
 			<!-- article -->
 			<article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
+				<h2><?php _e( 'Page not found', 'html5blank' ); ?></h2>
+				<h5>
 					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
+				</h5>
+				<?php get_template_part('partials/searchform'); ?>
 
 			</article>
 			<!-- /article -->
@@ -19,6 +20,12 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+	<!--Logo-->
+	<section>
+	    <figure id="logo-2">
+	        <a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_url'); ?>/assets/img/logo.png" alt="John Krummen for Council" /></a>
+	    </figure>
+	</section>
+
 
 <?php get_footer(); ?>
